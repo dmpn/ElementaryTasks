4c9434838d859171f04d6446cbea35e0d525e072
dmpn <dmpn@i.ua>
2021-05-23 18:19:47 +0300

    Добавьте файлы проекта.

---
+++ ChessBoard/Board.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    public class Board
+    {
+        internal void Print(int Height, int Width)
+        {
+           
+
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < Height; i++)
+            {
+                switch (i % 2)
+                {
+                    case 0:
+                        for (int j = 0; j < Width; j++)
+                        {
+                            switch (j % 2)
+                            {
+                                case 0:
+                                    Console.Write("*");
+                                    break;
+                                default:
+                                    Console.Write(" ");
+                                    break;
+                            }                                                                                          
+                        }
+                        Console.WriteLine();
+                        break;
+                    default:
+                        for (int j = 0; j < Width; j++)
+                        {
+                            switch (j % 2)
+                            {
+                                case 0:
+                                    Console.Write(" ");
+                                    break;
+                                default:
+                                    Console.Write("*");
+                                    break;
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+                    
+             
+            }
+    
+        }
+    }
+}
+
---
+++ ChessBoard/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            switch (args.Length)
+            {
+                case 0:
+                    Console.WriteLine("This application prints a chessboard.");
+                    Console.WriteLine("Start application with two integer args in range 1 - 100");
+                    break;
+                case 2:
+                    int Height = 0, Width = 0;
+                    if (int.TryParse(args[0], out Height) && int.TryParse(args[1], out Width))
+                    {
+                        if (0 < Height && Height <= 100 && 0 < Width && Width <= 100)
+                        {
+                            Board Board = new Board();
+                            Board.Print(Height, Width);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect params");
+                        }
+                    }
+                    else
+                        Console.WriteLine("Incorrect params"); 
+                    break;
+                default:
+                    Console.WriteLine("Incorrect params");
+                    break;
+            }
+           
+
+           
+        }
+    }
+}
+  
+       
